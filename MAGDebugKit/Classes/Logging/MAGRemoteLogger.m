94c92a8306fdc3876a758157a482e3cd83b48a0f
Alexander Gorbunov <gorbunov@magora-systems.com>
2017-07-04 11:07:28 +0700

    Implement persistent store of logs queue

--- MAGDebugKit/Classes/Logging/MAGRemoteLogger.m
+++ MAGDebugKit/Classes/Logging/MAGRemoteLogger.m
@@ -1,9 +1,43 @@
 #import "MAGRemoteLogger.h"
 #import <CocoaAsyncSocket/GCDAsyncSocket.h>
+#import <ReactiveObjC/ReactiveObjC.h>
 
 
 static NSTimeInterval const retryInterval = 10.0;
 
+
+NSDictionary *mag_encodedLogMessage(DDLogMessage *message) {
+	NSMutableDictionary *encoded = [[NSMutableDictionary alloc] init];
+	
+	encoded[@keypath(DDLogMessage.new, message)] = message.message;
+	encoded[@keypath(DDLogMessage.new, level)] = @(message.level);
+	encoded[@keypath(DDLogMessage.new, flag)] = @(message.flag);
+	encoded[@keypath(DDLogMessage.new, context)] = @(message.context);
+	encoded[@keypath(DDLogMessage.new, file)] = message.file;
+	encoded[@keypath(DDLogMessage.new, function)] = message.function;
+	encoded[@keypath(DDLogMessage.new, line)] = @(message.line);
+	encoded[@keypath(DDLogMessage.new, tag)] = message.tag;
+	encoded[@keypath(DDLogMessage.new, options)] = @(message.options);
+	encoded[@keypath(DDLogMessage.new, timestamp)] = message.timestamp;
+	
+	return encoded;
+}
+
+DDLogMessage *mag_decodedLogMessage(NSDictionary *encoded) {
+	DDLogMessage *message = [[DDLogMessage alloc]
+		initWithMessage:encoded[@keypath(DDLogMessage.new, message)]
+		level:[encoded[@keypath(DDLogMessage.new, level)] unsignedIntegerValue]
+		flag:[encoded[@keypath(DDLogMessage.new, flag)] unsignedIntegerValue]
+		context:[encoded[@keypath(DDLogMessage.new, context)] integerValue]
+		file:encoded[@keypath(DDLogMessage.new, file)]
+		function:encoded[@keypath(DDLogMessage.new, function)]
+		line:[encoded[@keypath(DDLogMessage.new, line)] unsignedIntegerValue]
+		tag:encoded[@keypath(DDLogMessage.new, tag)]
+		options:[encoded[@keypath(DDLogMessage.new, options)] integerValue]
+		timestamp:encoded[@keypath(DDLogMessage.new, timestamp)]];
+	
+	return message;
+}
 
 @interface MAGRemoteLogger () <GCDAsyncSocketDelegate>
 
@@ -11,9 +45,9 @@ static NSTimeInterval const retryInterval = 10.0;
 @property (nonatomic) NSUInteger port;
 @property (nonatomic) GCDAsyncSocket *socket;
 
-#warning Rewrite to make access to the queue thread-safe.
-@property (nonatomic) NSMutableArray *logsToShip;
+@property (nonatomic) NSMutableArray <DDLogMessage *> *logsToShip;
 @property (atomic) dispatch_queue_t loggingQueue;
+@property (nonatomic) NSURL *diskQueue;
 
 @property (nonatomic) DDLogMessage *shippingLog;
 
@@ -37,6 +71,14 @@ static NSTimeInterval const retryInterval = 10.0;
 	_port = port;
 	_socket = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:_loggingQueue];
 	
+	NSString *cachesDir = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES).firstObject;
+	NSString *fileId = [NSString stringWithFormat:@"%@:%@", host, @(port)];
+	NSData *fileIdData = [fileId dataUsingEncoding:NSUTF8StringEncoding];
+	NSString *fileName = [fileIdData base64EncodedStringWithOptions:0];
+	NSString *path = [cachesDir stringByAppendingPathComponent:fileName];
+	_diskQueue = [NSURL fileURLWithPath:path];
+	[self loadDiskQueue];
+	
 	return self;
 }
 
@@ -45,11 +87,34 @@ static NSTimeInterval const retryInterval = 10.0;
 - (void)logMessage:(DDLogMessage *)logMessage {
 	dispatch_async(self.loggingQueue, ^{
 		[self.logsToShip addObject:logMessage];
+		[self saveDiskQueue];
 		[self shipFromQueue];
 	});
 }
 
 #pragma mark - Private methods
+
+- (void)loadDiskQueue {
+	NSArray *diskQueue = [[NSArray alloc] initWithContentsOfURL:self.diskQueue];
+	if (diskQueue) {
+		for (NSDictionary *encoded in diskQueue) {
+			DDLogMessage *message = mag_decodedLogMessage(encoded);
+			[self.logsToShip addObject:message];
+		}
+		
+		[self shipFromQueue];
+	}
+}
+
+- (void)saveDiskQueue {
+	NSMutableArray *encodedLogs = [[NSMutableArray alloc] initWithCapacity:self.logsToShip.count];
+	for (DDLogMessage *message in self.logsToShip) {
+		NSDictionary *encoded = mag_encodedLogMessage(message);
+		[encodedLogs addObject:encoded];
+	}
+
+	[encodedLogs writeToURL:self.diskQueue atomically:YES];
+}
 
 - (void)shipFromQueue {
 	if (self.logsToShip.count == 0) {
@@ -83,6 +148,8 @@ static NSTimeInterval const retryInterval = 10.0;
 - (void)socket:(GCDAsyncSocket *)sock didWriteDataWithTag:(long)tag {
 	[self.logsToShip removeObject:self.shippingLog];
 	self.shippingLog = nil;
+	
+	[self saveDiskQueue];
 }
 
 - (void)socketDidDisconnect:(GCDAsyncSocket *)sock withError:(nullable NSError *)err {
