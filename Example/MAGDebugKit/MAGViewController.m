cf578b0fa0849e80eb815d9beb6811254def61dd
Alexander Gorbunov <gorbunov@magora-systems.com>
2017-07-06 18:38:33 +0700

    Add several buttons on the example app to test log levels

--- Example/MAGDebugKit/MAGViewController.m
+++ Example/MAGDebugKit/MAGViewController.m
@@ -2,12 +2,8 @@
 #import <CocoaLumberjack/CocoaLumberjack.h>
 #import <MAGDebugKit/MAGLogging.h>
 
-#import "MAGAutoVideoRecorder.h"
 
 @interface MAGViewController () <UITextFieldDelegate>
-
-@property (strong, nonatomic) IBOutlet UIButton *stopVideoRecordingButton;
-
 @end
 
 
@@ -15,8 +11,31 @@
 
 #pragma mark - UI events
 
-- (IBAction)buttonTap:(id)sender {
-	DDLogInfo(@"Button tap.");
+- (IBAction)verboseButtonTap:(id)sender {
+	DDLogVerbose(@"Verbose button tap.");
+}
+
+- (IBAction)debugButtonTap:(id)sender {
+	DDLogDebug(@"Debug button tap.");
+}
+
+- (IBAction)infoButtonTap:(id)sender {
+	DDLogInfo(@"Info button tap.");
+}
+
+- (IBAction)warningButtonTap:(id)sender {
+	DDLogWarn(@"Warning button tap.");
+}
+
+- (IBAction)errorButtonTap:(id)sender {
+	DDLogError(@"Error button tap.");
+}
+
+- (IBAction)crashButtonTap:(id)sender {
+	NSException *e = [NSException exceptionWithName:NSGenericException
+		reason:@"Exception button tap" userInfo:nil];
+	
+	@throw e;
 }
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
